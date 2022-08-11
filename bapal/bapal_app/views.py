28c4155fed09b5e00ff962c0055f908b4131ff7c
Swalbak <usually670@gmail.com>
2022-08-12 03:13:10 +0900

    fix #5

--- bapal/bapal_app/views.py
+++ bapal/bapal_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Test
+
+def test_view(request):
+    tests = Test.objects.all()
+
+    return render(request, 'bapal_app/index.html', {'tests': tests})
+
+def index(request):
+    return render(request, 'bapal_app/index.html')
